1c7f701bf3590638c7dfc10a81d19c09ab937182
Hugo <hugovk@users.noreply.github.com>
2020-02-13 11:46:20 +0200

    Replace ''machine learning' with 'thing labelling'

---
+++ machine-learning-to-thing-labelling/README.md
@@ -0,0 +1,13 @@
+'machine learning' to 'thing labelling'
+=======================================
+
+Chrome extension that replaces occurrences of 'machine learning' with 'thing labelling' (plus some variations).
+
+Why?
+
+See [Cassie Kozyrkov's talk on Decision Intelligence at Web Summit 2018](https://www.youtube.com/watch?v=xmwyTYAeAl4).
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions. Check 'Developer mode', then 'Load unpacked extension...' and navigate to this Source directory.
---
+++ machine-learning-to-thing-labelling/Source/content_script.js
@@ -0,0 +1,41 @@
+walk(document.body);
+
+function walk(node)
+{
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+
+  var child, next;
+
+  switch ( node.nodeType )
+  {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child )
+      {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
+}
+
+function handleText(textNode)
+{
+  var v = textNode.nodeValue;
+
+  v = v.replace(/\bmachine learning\b/g, "thing labelling");
+  v = v.replace(/\bMachine learning\b/g, "Thing labelling");
+  v = v.replace(/\bMachine Learning\b/g, "Thing Labelling");
+  v = v.replace(/\bMACHINE LEARNING\b/g, "THING LABELLING");
+  v = v.replace(/\bML\b/g, "TL");
+
+  textNode.nodeValue = v;
+}
